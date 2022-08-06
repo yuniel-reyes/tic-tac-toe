e9829dd4918560c0665d03a89b6887454b5a1ce1
Yuniel <yreyes8827@gmail.com>
2022-08-06 10:37:12 +0200

    Add PageBoard module It will create the gameboard: array inside of object It will render the content of the gameboard to the page

--- myscript.js
+++ myscript.js
@@ -1,3 +1,47 @@
-const gameBoard = (function(){
-    const _gameBoard = [];
+const PageBoard = (function(){
+
+    // Store 
+    const _GameBoard = {
+        gameBoard: [],
+    }
+
+    const gridContainer = document.getElementById('grid-container');
+
+    const _GRID_SIZE = 9;
+
+    const init = () => {
+        // _checkCacheDom();
+        _renderBoardGrid(gridContainer);
+        _populateBoard();
+    }
+    
+    const _populateBoard = () => {
+        const eachDiv = gridContainer.children;
+        const TicTac = "X"
+        for (divNum = 0; divNum < eachDiv.length; divNum++) {
+            _GameBoard.gameBoard.push(TicTac)
+            eachDiv[divNum].textContent = TicTac;
+        }
+    };
+    
+    const _checkCacheDom = () => {
+        gridContainer.textContent = "Hello World";
+    };
+
+    const _renderBoardGrid = (gridContainer) => {
+        for (let gridCounter = 1; gridCounter <= _GRID_SIZE; gridCounter++){
+            const div = document.createElement('div');
+            div.setAttribute('class', 'child')
+            gridContainer.appendChild(div);
+        }
+    };
+
+    init();
+
+    return {
+        _GameBoard,
+    };
+
 })();
+
+
--- requirements.md
+++ requirements.md
@@ -1,10 +1,23 @@
 ## Functional Requirements
 1. Create an object that stores the gameboard as 
 an array
+- gameBoard: see hint
+- create a function that renders the gameboard grid on 
+the page
+- create a function that populate the gameboard with the
+content of the array
+
 
 2. Create a factory function that return a player
 3. Create an object to control the flow of the game
+
 Hint: If you only ever need ONE of something (gameboard, 
       displayController), use a module
       - If you need multiples of something (players),
       create them with factories.
+
+
+
+## Non functional Requirements
+1. Set up the HTML to display the gameboard: HTML
+2. 1. Set up the HTML to display the gameboard: CSS

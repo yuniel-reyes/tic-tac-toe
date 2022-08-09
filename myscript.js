e3d929c9b13f2a328266b93784c9b1edfd6c1ed6
Yuniel <yreyes8827@gmail.com>
2022-08-09 16:34:54 +0200

    Add displayBoard module - It will get the grid-container node reference once for all - Create public method init() that will run for now _renderBoard   init() takes the current player object   init() runs _renderBoard passing current player object into it - Create _renderBoard   _renderBoard  renders the gameboard grid on the page:   add two attributes to each div element: a class and a data-*     (the data-* attr will allow me to track clicked div)   add event listener to each div

--- myscript.js
+++ myscript.js
@@ -8,3 +8,46 @@ const GameBoard = (function(){
     }
 
 })();
+// ================================
+
+// Module Pattern to render the board
+const displayBoard = (() => {
+
+    const gridContainer = document.getElementById('grid-container');
+    const _GRID_SIZE = 9;
+
+    const init = (eachPlayer) => {
+        _renderBoard(gridContainer, eachPlayer.addMark);
+        // _populateBoard(gridContainer);
+    };
+
+
+
+    function _renderBoard(gridContainer, addMark) {
+        for (let gridCounter = 0; gridCounter < _GRID_SIZE; gridCounter++) {
+            const div = document.createElement('div');
+            div.setAttribute('class', 'child');
+            div.setAttribute('data-cell', gridCounter);
+            div.addEventListener('mouseover', addMark);
+            gridContainer.appendChild(div);
+        }
+    }
+    
+    const _populateBoard = (gridContainer) => {
+        const arrFromColl = Array.from(gridContainer.children);
+        arrFromColl.forEach((div, indexOfDiv) => {
+            GameBoard.gameBoard.forEach((item, indexOfItem) => {
+                if (indexOfDiv == indexOfItem) {
+                    div.textContent = item;
+                }
+            })
+        });
+    };
+
+     
+    return {
+        init,
+        // gridContainer,
+    };
+
+})();

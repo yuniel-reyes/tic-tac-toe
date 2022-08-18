d34e18d45fb51c75a105e3d4ff5c0bf961dd2df7
Yuniel <yreyes8827@gmail.com>
2022-08-18 17:04:50 +0200

    Refactor - Put every module and factory function inside the Game module - Inside the displayBoard module:   - add event listener function checkPlayer()   - add updateArray()   - add updateBoardPage()   - add functionality to change player

--- myscript.js
+++ myscript.js
@@ -1,79 +1,106 @@
-// Module Pattern for Board  
-const GameBoard = (function(){
+// Module for Game flow
+const Game = (() => {
+
+    // Module Pattern for Gameboard  
+    const GameBoard = (() => {
+        
+        return {
+            gameBoard: ["?", "?", "?",
+                    "?", "?", "?",
+                    "?", "?", "?"],  
+        }
+
+    })();
+
+    // =================================
+
+    // Factory function for players
+    const Players = (id, mark) => {
+        id = id;
+        mark = mark;
     
-    return {
-        gameBoard: ["?", "?", "?",
-                   "?", "?", "?",
-                   "?", "?", "?"],  
-    }
+        return {
+            mark,
+        }
+};
 
-})();
-// ================================
+    // Create players
+    const player1 = Players(1, "X");
+    const player2 = Players(2, "0");
 
-// Module Pattern to render the board
-const displayBoard = (() => {
+    // Check and change player
+    let currentPlayer = player1;
+
+    // =================================
+
+    // Module Pattern to render the board
+    const displayBoard = (() => {
 
     const gridContainer = document.getElementById('grid-container');
     const _GRID_SIZE = 9;
 
-    const init = (eachPlayer) => {
-        _renderBoard(gridContainer, eachPlayer.addMark);
-        // _populateBoard(gridContainer);
-    };
+    //  Add mark to a particular spot in the gameboard array
+    const checkPlayer = (e) => {
+        
+        // update array
+        updateArray(e);
+        // update board on page
+        updatePageBoard(e);
+        // change currentPlayer for every click
+        currentPlayer = currentPlayer == player1 ? player2 : player1;
+        // check winner
+        checkWinner();        
+    }
+
+    const updatePageBoard = (e) => {
+        // update board on page
+        // check if that spot is empty
+        if (e.target.textContent === "") {
+            e.target.textContent = currentPlayer.mark;
+        }
+        return;
+    }
 
 
+    const updateArray = (e) => {
+        // check spot in the array and update if empty
+        if (GameBoard.gameBoard[e.target.dataset.cell] === "?") {
+            GameBoard.gameBoard[e.target.dataset.cell] = currentPlayer.mark;
+        }
+        return;
+    }
 
-    function _renderBoard(gridContainer, addMark) {
+    const checkWinner = () => {
+        // winner patterns
+        
+    }
+
+
+    const _renderBoard = (gridContainer, checkPlayer) => {
         for (let gridCounter = 0; gridCounter < _GRID_SIZE; gridCounter++) {
             const div = document.createElement('div');
             div.setAttribute('class', 'child');
             div.setAttribute('data-cell', gridCounter);
-            div.addEventListener('mouseover', addMark);
+            div.addEventListener('click', checkPlayer);
             gridContainer.appendChild(div);
         }
     }
+
     
-    const _populateBoard = (gridContainer) => {
-        const arrFromColl = Array.from(gridContainer.children);
-        arrFromColl.forEach((div, indexOfDiv) => {
+/*     const _populateBoard = (gridContainer) => {
+        const arrFromCollection = Array.from(gridContainer.children);
+        arrFromCollection.forEach((div, indexOfDiv) => {
             GameBoard.gameBoard.forEach((item, indexOfItem) => {
-                if (indexOfDiv == indexOfItem) {
+                if (item !== "?") {
                     div.textContent = item;
                 }
             })
         });
     };
+ */
+    _renderBoard(gridContainer, checkPlayer);
 
-     
-    return {
-        init,
-        // gridContainer,
-    };
 
-})();
-// ==========================================
-// Factory function for players
-const Players = (id, mark) => {
-    id = id;
-    mark = mark;
-
-    const addMark = () => {
-        console.log("It works")
-    };
- 
-    return {
-        addMark,
-    }
-};
-// ===================================
-// Module for Game flow
-const Game = (() => {
-
-    // Create player
-    const player1 = Players(1, "X");
-    // 
-    displayBoard.init(player1);
-    
-
+    })();
 })();
 
